049955aea0fc23b10f32d8197fd68292b90f8382
Aditya Sehgal <52095550+adsehgal@users.noreply.github.com>
2020-08-25 17:44:29 +0530

    changed MCU to atmega328p for easier arduino dev

--- Firmware/PSU_FW/src/main.cpp
+++ Firmware/PSU_FW/src/main.cpp
@@ -1,4 +1,13 @@
+//library includes
 #include <Arduino.h>
+
+//pin definitions
+#define I_PIN A0
+#define V_PIN A1
+#define UP_BTN 
+#define DW_BTN 4
+#define V_I_BTN 3
+#define O_EN_BTN 
 
 void setup() {
   // put your setup code here, to run once:
