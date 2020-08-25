b2dfdd32db2b6f6aedd3bdcab570610c550ad6a8
Aditya Sehgal <52095550+adsehgal@users.noreply.github.com>
2020-08-25 21:01:40 +0530

    added oled bootscreen

--- Firmware/PSU_FW/src/main.cpp
+++ Firmware/PSU_FW/src/main.cpp
@@ -4,6 +4,13 @@
 #include "MCP4023.h"
 #include "buttons.h"
 #include "analog.h"
+#include "graphics.h"
+#include "Adafruit_GFX.h"
+#include "Wire.h"
+#include "Adafruit_SSD1306.h"
+
+#define OLED_RESET 9
+Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
 
 
 #define OUT_LED A2
@@ -11,11 +18,31 @@
 
 #define nREG_EN 5
 
+#define OLED_ADD 0x3C //or 0x3D
+const uint16_t OLED_W = 128;
+const uint16_t OLED_H = 32;
+
 bool V_I_SEL = false; //false = I; true = V
 
+void displayLogo(){
+  display.clearDisplay();
+  display.drawBitmap(0, 0, BOOTSCREEN, OLED_W, OLED_H, WHITE);
+  display.display();
+}
+
+void displayCLReached(){
+  display.clearDisplay(); //for Clearing the display
+  display.drawBitmap(0, 0, CL_REACHED_W, OLED_W, OLED_H, WHITE);
+  display.display();
+  delay(300);
+  display.clearDisplay(); //for Clearing the display
+  display.drawBitmap(0, 0, CL_REACHED_B, OLED_W, OLED_H, WHITE);
+  display.display();
+  delay(300);
+}
 
 void setup() {
-
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADD); //or 0x3C
   pinMode(OUT_LED, OUTPUT);
   pinMode(CL_LED, OUTPUT);
   pinMode(nREG_EN, OUTPUT);
@@ -24,13 +51,14 @@ void setup() {
   initDiv();
   initBtns();
   initAnalog();
-  
+  displayLogo();
+  delay(500);
 }
 
 void loop() {
   uint8_t btnPress = whichBtn();
-  double voltage;
-  double current;
+  // double voltage;
+  // double current;
   if (btnPress && up){
     rDivIncrement();
   }
